308c8202a6e9e5dd15b72ae6aa85f54a85999261
Bret Stine <bstine@csumb.edu>
2017-09-01 19:15:31 +0000

    adding lab 1

--- index.php
+++ index.php
@@ -3,9 +3,27 @@
 <html>
 
 <head>
-     <title></title>
+     <title> Bret Stine: Personal Website </title>
      <meta charset="utf-8"/>
 </head>
+
+<style>
+       
+        body {
+            background-color: #DBDBDB !important;
+        }
+       
+        header, main, nav, footer {
+            text-align: center;
+        }
+       
+        li {
+           display: inline-block;
+           /* float: left; */
+        }
+       
+    </style>
+
 <body>
     
     <h1> Welcome to Bret's CST336 website! </h1>
