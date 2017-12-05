b46e0caebae79389f4629e3316bab661f4e895a9
Bret Stine <bstine@csumb.edu>
2017-12-05 22:05:43 +0000

    adding hw5

--- homework/homework_5/database.php
+++ homework/homework_5/database.php
@@ -3,8 +3,23 @@
     include '../../dbConnection.php';
     $conn = getDatabaseConnection();
     
-    $movie_title = $_GET['movie_title'];
-    $sql = "INSERT ONTO `db_movies`"
+    $movieTitle = $_GET['title'];
+    $movieGenre = $_GET['genre'];
+    $movieYear = $_GET['year'];
+    
+    $sql = "INSERT ONTO `db_movies` 
+            (movieTitle, movieGenre, movieYear, time)
+            VALUES 
+            (:title, :genre, :year, CURRENT_TIMESTAMP)";
+            
+    $namedParameters = array();
+    $namedParameters[':title'] = $movieTitle;
+    $namedParameters[':genre'] = $movieGenre;
+    $namedParameters[':year'] = $movieYear;
+    
+    $stmt = $conn->prepare($sql);
+    $stmt->execute($namedParameters);
+    $movies = $stmt->fetchAll(PDO::FETCH_ASSOC);
     
     
 ?>
